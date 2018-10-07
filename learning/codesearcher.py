92c389b92ac8f790e39a49a0255ca22c0a6adc02
Ling Chunyang <lingcy@pku.edu.cn>
2018-10-07 16:29:20 +0800

    add config file and update codesearcher

--- learning/codesearcher.py
+++ learning/codesearcher.py
@@ -1,37 +1,51 @@
+import configparser
+
 import torch
 import torch.nn as nn
-from models import CNNEncoder
-from configs import get_config
-from dataset import CSDataset
-import dataset
-from torch.utils.data import DataLoader
 import torch.optim as optim
+
+from models import HybridModule
+from data_preprocess.dataset import CodeSearchDataset
+from torch.utils.data import DataLoader
 
 class CodeSearcher:
     def __init__(self, conf):
         self.conf = conf
-        self.model = CNNEncoder(self.conf['conv1_kernels'], self.conf['conv1_size'],
-                                self.conf['pool1_target'], self.conf['conv2_kernels'],
-                                self.conf['conv2_size'], self.conf['pool2_size'],
-                                self.conf['fc1_units'], self.conf['fc2_units'])
+        self.trainset = CodeSearchDataset('../tmp/train.pk')
+        self.query_max_size = self.trainset.query_max_size
+        self.core_term_size = self.trainset.core_term_size
+        self.model = HybridModule(
+                self.query_max_size, self.core_term_size, 
+                self.conf['model']['core_term_embedding_size'],self.conf['model']['lstm_layers'],
+                self.conf['model']['lstm_hidden_size'], self.conf['model']['fc_hidden_size'])
 
     def train(self):
-        nb_epoch = self.conf['nb_epoch']
-        batch_size = self.conf['batch_size']
-        trainset = CSDataset()
-        data_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True,
-                                 num_workers=1) #collate_fn=dataset.collate_fn)
-        criterion = nn.BCEWithLogitsLoss()
-        optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+        nb_epoch = self.conf['train']['nb_epoch']
+        batch_size = self.conf['train']['batch_size']
+    
+        #data_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True,
+        #                         num_workers=1) #collate_fn=dataset.collate_fn)
+        optimizer = optim.Adam(self.model.parameters(), lr=self.conf['train']['lr']
+        train_size = len(self.trainset)
         for epoch in range(nb_epoch):
-            for img, label in data_loader:
-                logits = self.model(img)
-                loss = criterion(logits, label)
-                print(logits, loss)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            shuffle_index = torch.randperm(train_size).tolist
+            loss = 0
+            for i, index in enumerate(shuffle_index):
+                sample = self.trainset[i]
+                pos_sample = sample.pos_data
+                for neg_sample in sample.neg_data_list:
+                    loss += model(pos_sample, neg_sample)
+                    print(loss)
+                if i % 8 == 0:
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                    loss = 0
 
+def get_config():
+    config = configparser()
+    config.read('../conf/config.ini')
+    return config
 
 if __name__ == '__main__':
     conf = get_config()
--- learning/model.py
+++ learning/model.py
@@ -29,7 +29,8 @@ class HybridModule(nn.Module):
         seq_len, emb_size = x.size()
         x = x.unsqueeze(0)
         rnn_out, _ = self.rnn(x, None) # batch_sz x seq_len x hidden_sz*2
-        rnn_out = F.maxpool1d(rnn_out.transpose(1, 2), seq_len).sequeeze(2) # batch_sz x hidden_sz*2
+        #rnn_out = F.maxpool1d(rnn_out.transpose(1, 2), seq_len).sequeeze(2) # batch_sz x hidden_sz*2
+        rnn_out = rnn_out[:, -1, :] # use the last output state, batch_sz x hidden_sz*2
         x = F.dropout(rnn_out, 0.25, self.training)
         x = rnn_out[:, -1, :]
         x = F.relu(self.fc_1(x))
